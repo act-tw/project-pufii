fcfd5797f01a076c48c755874a0661890fe19d11
tommy <tooto1985@gmail.com>
2016-04-06 17:17:09 +0800

    modify test.js ok

--- js/test.js
+++ js/test.js
@@ -1,7 +1,12 @@
 $(function() {
 	$(window).load(function() {
-		$("img[src]").each(function(){
-			console.log($(this).attr("src"));
+		$("img[src]").each(function() {
+			var src = $(this).attr("src");
+			if (/\?t=/i.test(src)) {
+				src+="?t=" + Math.random().toString().substr(2);
+				$(this).attr("src", src);	
+			}
 		});
+		console.log("changed");
 	});
 });
