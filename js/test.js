9d4ca69a593996aa67568ad361ee52631bad2e84
tommy <tooto1985@gmail.com>
2016-04-06 17:19:02 +0800

    ok

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@ $(function() {
 	$(window).load(function() {
 		$("img[src]").each(function() {
 			var src = $(this).attr("src");
-			if (/\?t=/i.test(src)) {
+			if (!/\?t=/i.test(src)) {
 				src+="?t=" + Math.random().toString().substr(2);
 				$(this).attr("src", src);	
 			}
